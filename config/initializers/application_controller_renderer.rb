d3f7f020432d6ce11ed23e65032a0dc06ffbb545
hiroki-9 <bis.at.kinsu@gmail.com>
2020-12-26 05:01:43 +0000

    add initial app

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'c259ad7ca724c05d3dd1e738c6a294bb30a7b38aba29f61d24fe8e7744a11847ad0d4904cba43fdea7a2b80799be9548a6f73092b4bfb7f52dad972b69e58710'
